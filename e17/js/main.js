6a0957b1984fbea20d9a9a5590f03c6e2fcc5280
arobin4 <54602992+arobin4@users.noreply.github.com>
2019-11-18 22:10:08 -0500

    yesh

--- e17/js/main.js
+++ e17/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
    console.log('THE DOCUMENT IS WORKING');
 
    $('.emotion').click(function(){
-     $('h1').css('text-align','left');
+     $('.letter1').toggleClass('align-left');
   });
 
 });
